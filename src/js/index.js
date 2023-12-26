18667c53d197f89941566f0a09ec193f7cd8b93d
SpehtOlena <speht_olena@ukr.net>
2023-12-26 16:06:55 +0200

    pull

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
-// const test = (number) => {
-// 	let sum = 0;
-// 	for (let i = 0; i < number; i++) {
-// 		if (i % 3 === 0 || i % 5 === 0) {
-// 			sum += i;
-// 		}
-// 	}
-// 	return sum
-// }
+const test = (number) => {
+	let sum = 0;
+	for (let i = 0; i < number; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			sum += i;
+		}
+	}
+	return sum
+}
 
-// console.log(test(16));
+console.log(test(16));
 
 
 const newTest = (number) => {
